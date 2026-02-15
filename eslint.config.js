e4b4176d65182d5f8309095f832fd8000faee4f3
VB2007 <admin@vb2007.hu>
2026-02-15 18:41:30 +0100

    created eslint config file

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const { ESLint } = require("@typescript-eslint/utils");
+
+module.exports = [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unsafe-assignment": "error",
+      "prefer-const": "error",
+      "no-var": "error",
+    },
+  },
+];
