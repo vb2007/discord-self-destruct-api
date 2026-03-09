a9a706aaaf7f1311da5e782bfd7da384f1fe41d8
VB2007 <admin@vb2007.hu>
2026-03-09 04:14:03 +0100

    added prisma adapter & client export

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "./generated/prisma";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "",
+});
+
+export const prisma = new PrismaClient({ adapter });
