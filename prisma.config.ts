8f75bb139ccd9d4e80bd1812604584b2268fe05b
VB2007 <admin@vb2007.hu>
2026-04-09 15:43:38 +0200

    updated .env usage order in prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,19 +8,19 @@ try {
 
 const {
   DATABASE_HOST_ADDRESS,
+  DATABASE_NAME,
   DATABASE_USER,
   DATABASE_PASSWORD,
-  DATABASE_NAME,
 } = process.env;
 
 if (
   !DATABASE_HOST_ADDRESS ||
+  !DATABASE_NAME ||
   !DATABASE_USER ||
-  !DATABASE_PASSWORD ||
-  !DATABASE_NAME
+  !DATABASE_PASSWORD
 ) {
   throw new Error(
-    "Missing required environment variables: DATABASE_HOST_ADDRESS, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME",
+    "Missing required environment variables: DATABASE_HOST_ADDRESS, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD",
   );
 }
 
