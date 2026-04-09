f556dde96c68bf466a6ce1847d9054e37adf24eb
VB2007 <admin@vb2007.hu>
2026-04-09 12:14:16 +0200

    imported core packages, installed dev & runtime dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
 import { PrismaClient } from "../generated/prisma";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
